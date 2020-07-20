5c33c835d4505dbb8ae8c06e5bfc0e82cf2a3398
Mingli Yuan <mingli.yuan@gmail.com>
2020-07-20 11:13:03 +0800

    add ski

--- src/main/java/ski/SKI.java
+++ src/main/java/ski/SKI.java
@@ -7,7 +7,7 @@ interface SKI {
 
     interface Combinator {
         String script();
-        Context stream(int depth);
+        Context stream(int maxdepth, int curdepth);
         Combinator evaluate();
     }
 
@@ -16,6 +16,7 @@ interface SKI {
     class CompositiveCombinator implements Combinator {
         public final Combinator left;
         public final Combinator right;
+        private int counter = 0;
 
         CompositiveCombinator(Combinator left, Combinator right) {
             this.left = left;
@@ -28,26 +29,35 @@ interface SKI {
         }
 
         @Override
-        public Context stream(int depth) {
+        public Context stream(int maxdepth, int curdepth) {
+            if (curdepth == 0) {
+                counter = 0;
+            }
+            if (curdepth < maxdepth) {
+                Context ctx = stream(maxdepth, curdepth + 1);
+            } else {
+
+            }
             return null;
         }
 
         @Override
         public Combinator evaluate() {
-            Context ctx = this.stream(1);
+            Context ctx = this.stream(1, 0);
             return switch(ctx.script) {
                 case "(I, $1)" -> ctx.references.get("$1").evaluate();
+                case "(ι, $1)" -> combine(combine(ctx.references.get("$1").evaluate(), S()), K());
                 default  -> {
-                    ctx = this.stream(2);
+                    ctx = this.stream(2, 0);
                     yield switch (ctx.script) {
                         case "((K, $1), $2)" -> ctx.references.get("$1").evaluate();
                         default -> {
-                            ctx = this.stream(3);
+                            ctx = this.stream(3, 0);
                             yield switch (ctx.script) {
                                 case "(((S, $1), $2), $3)" -> {
-                                    Combinator $1 = ctx.references.get("$1");
-                                    Combinator $2 = ctx.references.get("$2");
-                                    Combinator $3 = ctx.references.get("$3");
+                                    Combinator $1 = ctx.references.get("$1").evaluate();
+                                    Combinator $2 = ctx.references.get("$2").evaluate();
+                                    Combinator $3 = ctx.references.get("$3").evaluate();
                                     yield combine(combine($1, $3), combine($2, $3));
                                 }
                                 default -> this;
@@ -72,8 +82,8 @@ interface SKI {
             }
 
             @Override
-            public Context stream(int depth) {
-                return null;
+            public Context stream(int maxdepth, int curdepth) {
+                return new Context("S", null);
             }
 
             @Override
@@ -92,8 +102,8 @@ interface SKI {
             }
 
             @Override
-            public Context stream(int depth) {
-                return null;
+            public Context stream(int maxdepth, int curdepth) {
+                return new Context("K", null);
             }
 
             @Override
@@ -112,8 +122,8 @@ interface SKI {
             }
 
             @Override
-            public Context stream(int depth) {
-                return null;
+            public Context stream(int maxdepth, int curdepth) {
+                return new Context("I", null);
             }
 
             @Override
@@ -132,8 +142,8 @@ interface SKI {
             }
 
             @Override
-            public Context stream(int depth) {
-                return null;
+            public Context stream(int maxdepth, int curdepth) {
+                return new Context("ι", null);
             }
 
             @Override
